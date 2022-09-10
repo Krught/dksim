387d249e500a02702154e7eb31a55ef7c8449d46
Remour <A.Krufka@aol.com>
2022-09-10 14:32:27 -0400

    Finished Style Loading Icon

--- web/dksim.py
+++ web/dksim.py
@@ -1432,7 +1432,6 @@ server.layout = html.Div(children=[
     html.Div(
         children=[
             dcc.Input(id="inputusername", type="text", placeholder="Username", debounce=True),
-            html.Br(),
         ],  style={"display": "flex", "justifyContent": "center"}),
     html.Div(children=[
         dcc.Loading(
