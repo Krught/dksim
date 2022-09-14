57d946e7801be123888f6ae73c174be7b9d2175a
Remour <A.Krufka@aol.com>
2022-09-14 19:31:54 -0400

    Update dksim.py

--- web/dksim.py
+++ web/dksim.py
@@ -1902,7 +1902,7 @@ loggin.layout = html.Div(children=[
             html.Button('Load Raw Log Text', id='submit_val_raw'),
             html.Br(),
             html.Button('Close Specific Log', id='close_val',),
-        ],  style={"display": "flex", "justifyContent": "center"}),
+        ],  style={"justifyContent": "center"}),
         html.Div(id='new-test-dash-container'),
         ])
 @loggin.callback(
