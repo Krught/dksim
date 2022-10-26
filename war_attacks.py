b57c0d988ee322298310f9ba41667a9264d607bb
Andrew Krufka <A.Krufka@aol.com>
2022-10-26 16:58:02 +0000

    Added warrior attacks

---
+++ war_attacks.py
@@ -0,0 +1,36 @@
+# These functions should only return the damage&stuff for rotation such as damage/status(hit/miss/crit)/abilityname
+#main function can do rage gen/weird on used / and rotation time
+def bloodthirst():
+
+#from war_attacks import *
+#copy that code to main file
+
+def whirlwind():
+
+def heroicstrike():
+
+def cleave():
+
+def execute():
+
+def slam():
+
+def rend():
+
+def mortalstrike():
+
+def overpower():
+
+def shatteringthrow():
+
+def sunderarmor():
+
+def bladestorm():
+
+def sweepingstrikes():
+
+def thunderclap():
+
+def bleeds():
+
+def deepwounds():
