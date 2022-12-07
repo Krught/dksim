c3a0bf8a70dda8cb348453b91a8cf61082b15b2a
Remour <109763026+Krught@users.noreply.github.com>
2022-12-07 16:14:22 -0500

    test

--- web/dksim.py
+++ web/dksim.py
@@ -1178,7 +1178,7 @@ def all_dash_stuff(dash_all_data):
     counts1, bins1 = np.histogram(extra_all_dps_results, bins=range(extra_all_dps_results_min, extra_all_dps_results_max, 50))
     bins1 = 0.5 * (bins1[:-1] + bins1[1:])
     fig_250_title = str("DPS Breakdown for All ") + str(total_amount_of_simss) + str(" Simulations")
-    fig250 = px.bar(x=bins1, y=counts1, labels={'x':'DPS', 'y':'Occurrence'}, title=fig_250_title, template="plotly_dark", color=counts1, color_continuous_scale="sunset", hovertext=[x, y]])
+    fig250 = px.bar(x=bins1, y=counts1, labels={'x':'DPS', 'y':'Occurrence'}, title=fig_250_title, template="plotly_dark", color=counts1, color_continuous_scale="sunset", hovertext=[x, y])
     fig250.update_layout(coloraxis_showscale=False)
     #fig250 = px.histogram(x=bins1, y=counts1, labels={'x':'DPS', 'y':'Occurrence'}, title=fig_250_title, template="plotly_dark", marginal="box")
     #extra_stats_split_list = extra_sim_stats_info.replace("*^*",", ")
