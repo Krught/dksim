e2c2d382828f7f3be9c9da53cb6daac1cddef868
Remour <109763026+Krught@users.noreply.github.com>
2022-12-22 22:08:55 -0500

    Exported Python Files

---
+++ web/sims/shared/weapon_roll.py
@@ -0,0 +1,6 @@
+#Math Function Area
+#Roll the selected weapon for a number in the range
+import random
+def weapon_roll(low_damage, high_damage):
+    roll = random.randint(low_damage, high_damage)
+    return roll
